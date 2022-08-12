952434b6b165b9ff2f5d9f278d1cb820c7418c22
Philip Case <philipbcase@gmail.com>
2022-08-12 16:09:42 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    const [data, setData] = React.useState(null);
+    const [loaded, setLoaded] = React.useState(false);
+  
+    React.useEffect(() => {
+      async function getData() {
+        const response = await fetch('./movies.json');
+        const json = await response.json();
+        setData(json);
+        setLoaded(true);
+      }
+      getData();
+    }, []);
+    console.log('loaded:', loaded, 'data:', data);
+  
+    return (
+      <>
+        <div className="container">
+          <div className="col-sm">
+            {loaded &&
+              data.movies.map((movie, i) => (
+                // Step 3 - Rename '<nexttech-movie' to match the name of your new React Component in 'movies.js'
+                <Movie
+                  // Do NOT remove this key attribute
+                  key={i}
+                  // Step 2 - Replace all of the attributes below with a single data={movie} attribute
+                  data={movie}
+                />
+              ))}
+          </div>
+        </div>
+      </>
+    );
+  }
+  
+  ReactDOM.render(<App />, document.getElementById('root'));
+  
